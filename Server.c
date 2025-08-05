ffe70cdc691feb9eb1f75df13cb2119b1b30ca97
SRIVIGHNATEJA <srivighnateja.vas55@gmail.com>
2025-08-05 11:10:45 +0530

    use pthreads to handle multiple clients concurrently

--- Server.c
+++ Server.c
@@ -2,15 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
 #define BUFFER_SIZE 1024
+
+void *handle_client(void *arg) {
+    int client_socket = *((int *)arg);
+    char buffer[BUFFER_SIZE];
+
+    send(client_socket, "Welcome to chat server\n", 23, 0);
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes <= 0) {
+            break;
+        }
+        send(client_socket, buffer, bytes, 0);
+    }
+
+    close(client_socket);
+    pthread_exit(NULL);
+}
 
 int main() {
     int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
-    char buffer[BUFFER_SIZE];
+    pthread_t tid;
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     server_addr.sin_family = AF_INET;
@@ -29,11 +49,7 @@ int main() {
             continue;
         }
 
-        memset(buffer, 0, BUFFER_SIZE);
-        recv(client_socket, buffer, sizeof(buffer), 0);
-        printf("Client connected\n");
-        send(client_socket, "Connected to chat server\n", 25, 0);
-        close(client_socket);
+        pthread_create(&tid, NULL, handle_client, &client_socket);
     }
 
     close(server_socket);
