3396da9bbd1e987857bde7fdba6bc3a6016c89f7
SRIVIGHNATEJA <srivighnateja.vas55@gmail.com>
2025-08-08 17:50:15 +0530

    implement private messaging between users

--- Server.c
+++ Server.c
@@ -38,6 +38,17 @@ void broadcast_message(const char *message, int exclude_socket) {
     pthread_mutex_unlock(&clients_mutex);
 }
 
+void private_message(const char *message, const char *recipient) {
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < num_clients; i++) {
+        if (strcmp(clients[i].username, recipient) == 0 && clients[i].logged_in) {
+            send(clients[i].socket, message, strlen(message), 0);
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
+}
+
 int handle_registration_login(int client_socket, char *username) {
     char buffer[BUFFER_SIZE], password[PASSWORD_LEN];
     int choice;
@@ -116,7 +127,7 @@ void *handle_client(void *arg) {
 
     while (1) {
         clear_buffer(buffer);
-        sprintf(buffer, "1. Broadcast message\n2. Exit\n");
+        sprintf(buffer, "1. Broadcast message\n2. Private message\n3. Exit\n");
         send(client_socket, buffer, strlen(buffer), 0);
 
         clear_buffer(buffer);
@@ -132,6 +143,21 @@ void *handle_client(void *arg) {
             char message[BUFFER_SIZE];
             snprintf(message, sizeof(message), "[%s]: %s", username, buffer);
             broadcast_message(message, client_socket);
+        } else if (choice == 2) {
+            send(client_socket, "Enter recipient username: ", 26, 0);
+            char recipient[USERNAME_LEN];
+            clear_buffer(recipient);
+            recv(client_socket, recipient, sizeof(recipient), 0);
+            recipient[strcspn(recipient, "\n")] = '\0';
+
+            send(client_socket, "Enter message: ", 15, 0);
+            clear_buffer(buffer);
+            recv(client_socket, buffer, sizeof(buffer), 0);
+            buffer[strcspn(buffer, "\n")] = '\0';
+
+            char message[BUFFER_SIZE];
+            snprintf(message, sizeof(message), "[%s to %s]: %s", username, recipient, buffer);
+            private_message(message, recipient);
         } else {
             break;
         }
